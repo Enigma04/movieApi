b199241c8d6cda336c2cb8d52d98a5c41a2cb26a
enigma <rohitvarma04@hotmail.com>
2022-01-30 21:52:41 +0530

    basic authentication

--- authentication.py
+++ authentication.py
@@ -1,26 +1,18 @@
 from fastapi import *
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-from database import user_list
-from models import User
-from schemas import users_serializer
-
 authentication = APIRouter()
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-
-@authentication.post('/token')
-async def token_generate(form_data: OAuth2PasswordRequestForm = Depends()):
-    return {'access_token': form_data.username + "'s token", "access_type": "bearer"}
+email = "rohit@gmail.com"
+password = "admin123"
 
 
 @authentication.post('/login')
-async def login(form_email: OAuth2PasswordRequestForm = Depends(oauth2_scheme),
-                form_password: OAuth2PasswordRequestForm = Depends(oauth2_scheme)):
-    email = users_serializer(user_list.find_one({"email": User.email}))
-    password = users_serializer(user_list.find_one({"password": User.password}))
-    if not email == form_email:
+async def login(form_email: OAuth2PasswordRequestForm = Depends(),
+                form_password: OAuth2PasswordRequestForm = Depends()):
+    if form_email.username != email :
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Incorrect user email')
-    if not password == form_password:
+    if form_password.password != password:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Incorrect user password')
-    return {"status": "ok", "details": f"Welcome! {User.name} "}
+    return {"status": "ok", "details": f"Welcome! {form_email.username} "}
--- schemas.py
+++ schemas.py
@@ -1,4 +1,3 @@
-from pydantic import BaseModel
 
 
 def movie_serializer(movie) -> dict:
